f58ba1b558299742584c6ee1d7b3da915a8b3a90
Jack K Lambert <jacklmbrt07@gmail.com>
2021-01-12 20:52:57 +0000

    hmmmm

--- routes/api.js
+++ routes/api.js
@@ -13,6 +13,16 @@ module.exports = function (app) {
     
   app.route('/api/solve')
     .post((req, res) => {
-
+      // Test #2
+      let puzzle = req.body.puzzle;
+      let solution = solver.solve(input)
+      let solutionObject = {puzzle, solution}
+      //Test 3
+      if (!puzzle){
+        return res.send({error: 'Required field missing'})
+      }
+      //test 4
+      //else if (regex){ return res.send({error: "Invalid characters in puzzle"})}
+      return res.json(solutionObject)
     });
 };
