dcba1af2fd311be7ce685f559f890f8a6befb654
Jack K Lambert <jacklmbrt07@gmail.com>
2021-01-13 21:16:29 +0000

    test 1 thru 5

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -16,6 +16,21 @@ class SudokuSolver {
   }
 
   solve(puzzleString) {
+    /*
+    1. If there is no empty cell in the matrix M:
+    return true
+    2. Let (i, j) be an empty cell in the matrix M
+    3. For i from 1 to 9:
+        3.1. If i is not present in the row r, in column c, and the 3x3
+        submatrix of (r, c):
+            a) M(r, c) = i 
+            b) recursively try fill in remaining empty cells
+            c) If recursion was successful:
+                return true
+            d) M(r, c) = 0
+    4. return false
+    */
+  
     return puzzleString;
   }
 }
--- routes/api.js
+++ routes/api.js
@@ -15,14 +15,22 @@ module.exports = function (app) {
     .post((req, res) => {
       // Test #2
       let puzzle = req.body.puzzle;
-      let solution = solver.solve(input)
-      let solutionObject = {puzzle, solution}
+      let regex = /[^1-9.]/g.test(puzzle)
+      let solution = solver.solve(puzzle)
       //Test 3
       if (!puzzle){
-        return res.send({error: 'Required field missing'})
+        return res.json({error: 'Required field missing'})
+      // Test 4
+      } else if (regex){ 
+        return res.json({error: 'Invalid characters in puzzle'})
+      // Test 5
+      } else if (puzzle.length != 81) { 
+        return res.json({error: 'Expected puzzle to be 81 characters long'})
+      // Test 6
+      } else if (!solution) {
+        return res.json({ error: "Puzzle cannot be solved"})
+      } else {
+        return res.json({solution})
       }
-      //test 4
-      //else if (regex){ return res.send({error: "Invalid characters in puzzle"})}
-      return res.json(solutionObject)
     });
 };
